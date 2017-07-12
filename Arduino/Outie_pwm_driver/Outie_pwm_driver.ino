f0c33bf3180f767b923957d17b8cf218497ca66b
OMSI\thudson <hydronics@gmail.com>
2017-07-12 16:05:48 -0700

    Signed-off-by: OMSI\thudson <hydronics@gmail.com>

---
+++ Arduino/Outie_pwm_driver/Outie_pwm_driver.ino
@@ -0,0 +1,100 @@
+/*
+ * LOTs Arduino PWM shield
+ * Raspberry PI sends pin ID and PWM rate over serial data via USB cable
+ * parsed into individual signals and drives mosfets with a PWM signal using the sparkfun mosfet shield
+ * https://www.sparkfun.com/products/10618
+ * 
+ * 1:30 is pin 1, PWM 30
+ * also accepts two or more values comma seperated up to 38 bytes
+ * 1:30,5:254,9:75
+ * terminated wiht a carraige return '/n'
+ * 
+ * Arduino PWM pins are 3,5,6,9,10,11
+ */
+
+// pins for the PWM circuits
+const int pwmPin3 = 3;
+const int pwmPin5 = 5;
+const int pwmPin6 = 6;
+const int pwmPin9 = 9;
+const int pwmPin10 = 10;
+const int pwmPin11 = 11;
+
+const int sizeOfInData = 40;
+char inData[sizeOfInData];
+byte index = 0;
+
+char *pwm1, *pwm2, *pwm3, *pwm4, *pwm5, *pwm6, *i;
+
+void setup() {  
+  Serial.begin(115200);
+  Serial.setTimeout(100);
+  // make the pins outputs:
+  pinMode(pwmPin3, OUTPUT);
+  pinMode(pwmPin5, OUTPUT);
+  pinMode(pwmPin6, OUTPUT);
+  pinMode(pwmPin9, OUTPUT);
+  pinMode(pwmPin10, OUTPUT);
+  pinMode(pwmPin11, OUTPUT);
+}
+void loop() {
+  while(Serial.available() > 0){
+    char aChar = Serial.read();
+      if(aChar == '\n')
+      {
+        for(int i = 0; i < index; i++){
+          Serial.write(inData[i]);
+        }
+        Serial.println();
+          char* command = strtok(inData, ",");
+          while (command != 0)
+          {
+            // Split the command in two values
+            char* separator = strchr(command, ':');
+            if (separator != 0){
+              // Actually split the string in 2: replace ':' with 0
+              *separator = 0;
+              int pinId = atoi(command);
+              ++separator;
+              int pwmRate = atoi(separator);
+
+              switch (pinId){
+                case 3:    // 
+                  analogWrite(pwmPin3, pwmRate);
+                  break;
+                case 5:    // 
+                  analogWrite(pwmPin5, pwmRate);
+                  break;
+                case 6:    //
+                  analogWrite(pwmPin6, pwmRate);
+                  break;
+                case 9:    // 
+                  analogWrite(pwmPin9, pwmRate);
+                  break;
+                case 10:    // 
+                  analogWrite(pwmPin10, pwmRate);
+                  break;
+                case 11:    // 
+                  analogWrite(pwmPin11, pwmRate);
+                  break;                    
+              }
+              Serial.print("pinId: ");
+              Serial.println(pinId);
+              Serial.print("pwmRate: ");
+              Serial.println(pwmRate);
+            }
+            // Find the next command in input string
+            command = strtok(0, "&");
+          }
+        index = 0;
+        inData[index] = NULL;
+      }else{
+        if(index < sizeOfInData-2){  //safety to keep from overrunning the buffer
+          inData[index] = aChar;
+          index++;
+        inData[index] = '\0'; // Keep the string NULL terminated
+      }
+    }
+  }
+  
+}
---
+++ raspberry pi/pi_write_to_arduino.py
@@ -0,0 +1,30 @@
+#find arduino serial using command: ls /dev/tty*
+#might be dev/ttyACM0 or ttyACM1 or ttyUSB0
+
+#Disable Auto Reset on Serial connection:
+#Stick a 120 ohm resistor in the headers between 5v and reset on the arduino
+
+
+#!/usr/bin/env python
+import serial
+
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate = 115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+    )
+
+print "Serial is open: " + str(ser.isOpen())
+
+print "Now Writing"
+ser.write("6:12") #pin 6, PWM 100
+ser.write('\n')
+
+print "Did write, now read"
+x = ser.readline()
+print "got '" + x + "'" #got '6:100'
+
+ser.close()
